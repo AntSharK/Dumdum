e748e77b7a1a543437e83431765ce34c27d5deb9
Anthony Moh <great_ant@hotmail.com>
2022-12-23 21:11:16 -0800

    Setup hub

--- Hubs/SwollBallHub.cs
+++ Hubs/SwollBallHub.cs
@@ -4,5 +4,20 @@ namespace Dumdum.Hubs
 {
     public class SwollBallHub : Hub
     {
+        /// <inheritdoc />
+        public override async Task OnConnectedAsync()
+        {
+            await Clients.Caller.SendAsync("FreshConnection");
+            await base.OnConnectedAsync();
+        }
+
+        /// <summary>
+        /// Creates a room in the lobby
+        /// </summary>
+        /// <returns>A task</returns>
+        public async Task CreateRoom()
+        {
+            // TODO
+        }
     }
 }
