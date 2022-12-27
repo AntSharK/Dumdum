f2c8918ed0e6a0a6af356e6a04f352b733a9f446
Anthony Moh <great_ant@hotmail.com>
2022-12-27 01:31:36 -0800

    Use this.playerball to load single ball

--- scripts/Swollball_Player_App.ts
+++ scripts/Swollball_Player_App.ts
@@ -15,7 +15,7 @@
                 },
 
                 backgroundColor: '#EEEEEE',
-                scene: [BallStats],
+                scene: [BallStats, BallUpgrades],
 
                 scale: {
                     autoCenter: Phaser.Scale.Center.CENTER_BOTH,
@@ -24,13 +24,33 @@
             });
     }
 }
+
+class BallUpgrades extends Phaser.Scene {
+    graphics: Phaser.GameObjects.Graphics;
+    playerBall: PlayerBall;
+    constructor() {
+        super({ key: 'BallUpgrades', active: true, visible: true });
+    }
+
+    preload() {
+        this.load.image('dummyimage', '/content/dummyimage.png');
+    }
+
+    create() {
+        this.graphics = this.add.graphics({ x: 0, y: 0 });
+    }
+
+    update() {
+    }
+}
+
 class BallStats extends Phaser.Scene {
     graphics: Phaser.GameObjects.Graphics;
-    playerBalls: PlayerBall[];
+    playerBall: PlayerBall;
     playerScore: ServerRoundScoreData;
     statsDisplay: Record<string, Phaser.GameObjects.Text>;
     constructor() {
-        super({ key: 'TestScene', active: false, visible: false });
+        super({ key: 'BallStats', active: true, visible: true });
     }
 
     preload() {
@@ -41,23 +61,21 @@ class BallStats extends Phaser.Scene {
         this.graphics = this.add.graphics({ x: 0, y: 0 });
         this.statsDisplay = {};
 
-        this.playerBalls = InitializeBalls(this.physics.add.group({
+        var playerBalls = InitializeBalls(this.physics.add.group({
             defaultKey: 'dummyimage',
             bounceX: 1,
             bounceY: 1,
         }), this);
+
+        this.playerBall = playerBalls[0];
 
         // Get the scale multiplier, so we know where to put things
         const ASSUMEDSCALE = 1000;
         var boundingDimension = Math.min(this.scale.canvas.width, this.scale.canvas.height);
         var scaleMultiplier = boundingDimension / ASSUMEDSCALE;
 
-        for (let playerBall of this.playerBalls) { // There should be only one ball
-            playerBall.setVelocity(0, 0); // Balls in this display do not move
-            playerBall.setPosition(this.scale.canvas.width / 2 - 300 * scaleMultiplier, this.scale.canvas.height / 2 - 210 * scaleMultiplier);
-        }
-
-        var playerBall = this.playerBalls[0];
+        this.playerBall.setVelocity(0, 0); // Balls in this display do not move
+        this.playerBall.setPosition(300 * scaleMultiplier, 300 * scaleMultiplier); // Set the ball to the top-left of the screen
 
         this.statsDisplay["hp"] = this.add.text(0, 0, "", { color: 'Black' });
         this.statsDisplay["dmg"] = this.add.text(0, 0, "", { color: 'Black' });
@@ -73,23 +91,20 @@ class BallStats extends Phaser.Scene {
             textArray.push(stat);
         }
 
-        Phaser.Actions.PlaceOnCircle(textArray, new Phaser.Geom.Circle(playerBall.x, playerBall.y - 35 * scaleMultiplier, playerBall.Size + 15 * scaleMultiplier), -0.8, 1.1);
+        Phaser.Actions.PlaceOnCircle(textArray, new Phaser.Geom.Circle(this.playerBall.x, this.playerBall.y - 35 * scaleMultiplier, this.playerBall.Size + 15 * scaleMultiplier), -0.8, 1.1);
     }
 
     update() {
-        if (this.playerBalls != null) {
-            this.updateText;
-        }
+        this.updateText();
         this.graphics.clear();
-        DrawBalls(this.graphics, this.playerBalls);
+        DrawBalls(this.graphics, [this.playerBall]);
     }
 
     updateText() {
-        var playerBall = this.playerBalls[0];
-        this.statsDisplay["hp"].text = "HP:" + playerBall.MaxHp.toString();
-        this.statsDisplay["dmg"].text = "DMG:" + playerBall.Damage.toString();
-        this.statsDisplay["armor"].text = "ARMOR:" + playerBall.Armor.toString();
-        this.statsDisplay["velocity"].text = "SPEED:" + playerBall.VelocityMultiplier.toString();
-        this.statsDisplay["size"].text = "SIZE:" + playerBall.SizeMultiplier.toString();
+        this.statsDisplay["hp"].text = "HP:" + this.playerBall.MaxHp.toString();
+        this.statsDisplay["dmg"].text = "DMG:" + this.playerBall.Damage.toString();
+        this.statsDisplay["armor"].text = "ARMOR:" + this.playerBall.Armor.toString();
+        this.statsDisplay["velocity"].text = "SPEED:" + this.playerBall.VelocityMultiplier.toString();
+        this.statsDisplay["size"].text = "SIZE:" + this.playerBall.SizeMultiplier.toString();
     }
 }
