bd490bb71afaa02465d9a29c26157a1fe727e77d
Anthony Moh <great_ant@hotmail.com>
2022-12-30 18:04:47 -0800

    Add BaseUpgrade class

--- Swollball/Upgrades/ArmorUpgrade.cs
+++ Swollball/Upgrades/ArmorUpgrade.cs
@@ -6,27 +6,20 @@ using System.Threading.Tasks;
 
 namespace Swollball.Upgrades
 {
-    public class ArmorUpgrade : IUpgrade
+    public class ArmorUpgrade : BaseUpgrade
     {
-        public string UpgradeName => $"ARMOR+{this.amount}";
-
-        public string Description => $"Increases armor by {this.amount}";
-
-        public string ServerId { get; private set; } = Guid.NewGuid().ToString();
-
-        public int BorderColor { get; private set; } = 11045079;
-
-        private int amount;
-
-        public ArmorUpgrade(int amount)
+        public ArmorUpgrade(int amount) : base(amount)
         {
-            this.amount = amount;
         }
 
-        public void PerformUpgrade(Ball ball)
+        public override string UpgradeName => $"ARMOR+{this.amount}";
+
+        public override string Description => $"Increases armor by {this.amount}";
+
+        public override void PerformUpgrade(Ball ball)
         {
             ball.Armor += this.amount;
-            ball.Upgrades.Add(this);
+            base.PerformUpgrade(ball);
         }
     }
 }
---
+++ Swollball/Upgrades/BaseUpgrade.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Swollball.Upgrades
+{
+    public abstract class BaseUpgrade : IUpgrade
+    {
+        public abstract string UpgradeName { get; }
+
+        public abstract string Description { get; }
+
+        public string ServerId { get; private set; } = Guid.NewGuid().ToString();
+
+        public virtual int BorderColor => 11045079;
+
+        internal int amount = 1;
+
+        public BaseUpgrade(int amount)
+        {
+            this.amount = amount;
+        }
+
+        public virtual void PerformUpgrade(Ball ball)
+        {
+            ball.Upgrades.Add(this);
+        }
+    }
+}
