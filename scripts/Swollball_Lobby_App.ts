fc5b347db7cb5c242599a589ebd83e05c74b3f6b
Anthony Moh <great_ant@hotmail.com>
2023-01-23 22:35:36 -0800

    Arena closes in (#62)
    * Shrink the arena

--- scripts/Swollball_Lobby_App.ts
+++ scripts/Swollball_Lobby_App.ts
@@ -60,7 +60,7 @@ class BallArena extends Phaser.Scene {
 
     preload() {
         this.load.image('dummyimage', '/content/dummyimage.png');
-        this.load.image('background', '/content/ui/circlearenaedit.png');
+        this.load.image('background', '/content/ui/circlearenalines.png');
     }
 
     create() {
@@ -132,6 +132,7 @@ class BallArena extends Phaser.Scene {
     startBallsMoving() {
         this.circlesMoving = true;
         SetBallVelocity(this.playerBalls, this);
+        ShrinkArena(this.arena, this.roundTimer.getRemainingSeconds() * 4); // Shrink the arena to 75% the radius by the end of the round
     }
 
     update() {
@@ -254,7 +255,20 @@ class Arena {
     }
 }
 
+function ShrinkArena(arena: Arena, timeToDisappear: number) {
+    arena.PhysicsGroup.children.each(function (b) {
+        var xDiff = arena.XPos - (<Phaser.Physics.Arcade.Sprite>b).x;
+        var yDiff = arena.YPos - (<Phaser.Physics.Arcade.Sprite>b).y;
+        var direction = new Phaser.Math.Vector2(xDiff, yDiff);
+        (<Phaser.Physics.Arcade.Sprite>b).setVelocity(direction.x / timeToDisappear, direction.y / timeToDisappear);
+    });
+}
+
 function DrawArena(graphics: Phaser.GameObjects.Graphics, arena: Arena) {
-    graphics.lineStyle(8, 0x000000);                        
-    graphics.strokeCircle(arena.XPos, arena.YPos, arena.Radius + 4)
+    var xDiff = (<Phaser.Physics.Arcade.Sprite>arena.PhysicsGroup.children.entries[0]).x - arena.XPos;
+    var yDiff = (<Phaser.Physics.Arcade.Sprite>arena.PhysicsGroup.children.entries[0]).y - arena.YPos;
+    var drawnRadius = Math.sqrt(xDiff * xDiff + yDiff * yDiff);
+
+    graphics.lineStyle(8, 0x000000);
+    graphics.strokeCircle(arena.XPos, arena.YPos, drawnRadius + 4)
 }
