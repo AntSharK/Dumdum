2461e62b9a412aa9dbde4d02914776f5e01942dd
Anthony Moh <great_ant@hotmail.com>
2023-01-31 23:46:18 -0800

    Add status page

---
+++ Pages/SwollballInsight.cshtml.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Dumdum.Pages
+{
+    public class SwollballInsightModel : PageModel
+    {
+        public Swollball.GameRoom Room;
+
+        public void OnGet([FromServices] Swollball.Lobby lobby,
+            [FromQuery] string roomId)
+        {
+            if (roomId == null || !lobby.Rooms.ContainsKey(roomId))
+            {
+                return;
+            }
+
+            Room = lobby.Rooms[roomId];
+        }
+    }
+}
