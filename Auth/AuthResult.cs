62e6d7c47f5531eac4c41badd825fd15c4462195
Anthony Moh <great_ant@hotmail.com>
2023-02-13 21:23:21 -0800

    Add sql deps
    Add MSA Auth
    
    Sample auth
    
    Fix redirect for consumer
    
    Auth works
    
    Mock connection
    
    READ FROM SQL
    
    Add AuthResult object
    
    Add secrets for backend upn/pw
    
    Some stuff for prod auth

---
+++ Auth/AuthResult.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using System.Security.Claims;
+
+namespace Dumdum.Auth
+{
+    public class AuthResult
+    {
+        public bool IsAuthenticated { get; private set; } = false;
+        private readonly string? name;
+        private readonly ClaimsIdentity? claimsIdentity;
+
+        private const string RATINGCLAIMKEY = "rating";
+
+        public AuthResult(ClaimsIdentity identity)
+        {
+            if (identity == null ||
+                identity.Claims == null ||
+                !identity.IsAuthenticated)
+            {
+                return;
+            }
+
+            this.IsAuthenticated = true;
+            this.claimsIdentity = identity;
+            this.name = identity.Name;
+        }
+
+        public string? Email
+        {
+            get
+            {
+                var emailClaim = this.claimsIdentity?.FindFirst(c => c.Type == ClaimTypes.Email);
+                if (emailClaim != null)
+                {
+                    return emailClaim.Value;
+                }
+
+                return this.name;
+            }
+        }
+
+        public int Rating
+        {
+            get
+            {
+                var ratingClaim = this.claimsIdentity?.FindFirst(c => c.Type == RATINGCLAIMKEY);
+                if (ratingClaim != null)
+                {
+                    return Int32.Parse(ratingClaim.Value);
+                }
+
+                return -1;
+            }
+            set
+            {
+                var ratingClaim = this.claimsIdentity?.FindFirst(c => c.Type == RATINGCLAIMKEY);
+                if (ratingClaim != null)
+                {
+                    this.claimsIdentity?.RemoveClaim(ratingClaim);
+                }
+
+                this.claimsIdentity?.AddClaim(new Claim(RATINGCLAIMKEY, value.ToString()));
+            }
+        }
+    }
+}
---
+++ Auth/SecretManager.cs
@@ -0,0 +1,24 @@
+﻿using Azure.Identity;
+using Microsoft.Extensions.Azure;
+
+namespace Dumdum.Auth
+{
+    internal static class SecretManager
+    {
+        internal static void Init(WebApplicationBuilder builder)
+        {
+            if (builder.Environment.IsProduction())
+            {
+                builder.Configuration.AddAzureKeyVault(
+                    new Uri($"https://antshark-vault.vault.azure.net/"),
+                    new DefaultAzureCredential());
+            }
+        }
+
+        internal static string GetSecret(string secretName, WebApplicationBuilder builder)
+        {
+            return builder.Configuration[secretName] ?? string.Empty;
+        }
+
+    }
+}
