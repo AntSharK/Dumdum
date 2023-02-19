547e15c3f2d85295626d40431b1705dd73cb3e67
Anthony Moh <great_ant@hotmail.com>
2023-02-19 01:38:18 -0800

    Move to CSHTML
    Include a custom build number
    
    Use Index CSHTML

---
+++ Pages/Swollball/Swollball_Host.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Dumdum.Pages.Swollball
+{
+    public class Swollball_HostModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
