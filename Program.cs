c4910b5a3e4f901f963f6ce9a5d68a18c8652f86
Anthony Moh <great_ant@hotmail.com>
2023-02-19 19:43:19 -0800

    Auth experiment (#104)
    * Add Azure AKV Connection
    
    * Add SQL DB
    
    * Add MSA Auth

--- Program.cs
+++ Program.cs
@@ -1,4 +1,13 @@
+using Dumdum.Auth;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+SecretManager.Init(builder);
+
+// Configure Auth in Builder
+MSAAuth.AddAuth(builder);
+
 builder.Services.AddSignalR();
 builder.Services.AddSingleton<Swollball.Lobby>();
 builder.Services.AddRazorPages();
@@ -8,9 +17,14 @@ var app = builder.Build();
 app.UseDefaultFiles();
 app.UseStaticFiles();
 
+// Configure Auth for App
+MSAAuth.ConfigureAuth(app);
+
 app.Logger.LogInformation("Starting up...");
 Swollball.GameHub.RegisterLogger(app.Logger);
+
 app.MapHub<Swollball.GameHub>("/swollBallHub");
+
 app.MapRazorPages();
 
 app.Run();
