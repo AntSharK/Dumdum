d36be1d640d65f8ae29f04c104681c288b204c7c
Anthony Moh <great_ant@hotmail.com>
2023-02-20 00:09:37 -0800

    For when everything finally works (#108)

--- Program.cs
+++ Program.cs
@@ -1,11 +1,10 @@
 using Dumdum.Auth;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Logging.ClearProviders();
-builder.Logging.AddConsole();
+SecretManager.Init(builder);
 
 // Configure Auth in Builder
-//MSAAuth.AddAuth(builder);
+MSAAuth.AddAuth(builder);
 
 builder.Services.AddSignalR();
 builder.Services.AddSingleton<Swollball.Lobby>();
@@ -17,15 +16,11 @@ app.UseDefaultFiles();
 app.UseStaticFiles();
 
 // Configure Auth for App
-//MSAAuth.ConfigureAuth(app);
+MSAAuth.ConfigureAuth(app);
 
 app.Logger.LogInformation("Starting up...");
 Swollball.GameHub.RegisterLogger(app.Logger);
-
-app.UseDeveloperExceptionPage();
 app.MapHub<Swollball.GameHub>("/swollBallHub");
 
 app.MapRazorPages();
-
-SecretManager.Init(builder);
 app.Run();
