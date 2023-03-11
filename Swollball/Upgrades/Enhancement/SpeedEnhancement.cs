d10631abfcc91dc45623a5042eb44d65a68e13ef
Anthony Moh <great_ant@hotmail.com>
2023-03-10 20:58:24 -0800

    Rescale odds

---
+++ Swollball/Upgrades/Enhancement/SpeedEnhancement.cs
@@ -0,0 +1,25 @@
+﻿namespace Swollball.Upgrades
+{
+    internal class SpeedEnhancement : BasePersistentUpgrade
+    {
+        public SpeedEnhancement(int value, int cost, string name, int duration) : base(value, cost, name, duration)
+        {
+            this.Tags.Add(UpgradeTags.SPEEDUPGRADE);
+            this.Tags.Add(UpgradeTags.ENHANCEMENT);
+        }
+
+        public override string Description => $"Speed upgrades purchased are {this.UpgradeAmount}x better.";
+
+        public override int BorderColor => UpgradeColors.BLUE;
+        public override int FillColor => UpgradeColors.ROSE;
+
+        public override void AnotherUpgradePurchased(IUpgrade upgrade)
+        {
+            base.AnotherUpgradePurchased(upgrade);
+            if (upgrade.Tags.Contains(UpgradeTags.SPEEDUPGRADE))
+            {
+                upgrade.UpgradeAmount *= 2;
+            }
+        }
+    }
+}
