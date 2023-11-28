a6d1ac5b9c0371c202aaba54df5369b11f092fa4
Anthony Moh <great_ant@hotmail.com>
2023-11-28 02:42:40 -0800

    Initial zombbomb setup

---
+++ scripts/Zombbomb/Zombbomb_Lobby_App.ts
@@ -0,0 +1,59 @@
+﻿/* 
+GAME SCENES
+ * */
+class Zombbomb_Lobby_Game {
+    game: Phaser.Game;
+    constructor() {
+        this.game = new Phaser.Game(
+            {
+                width: "100%",
+                height: "97%",
+                type: Phaser.AUTO,
+
+                physics: {
+                    default: 'arcade',
+                    arcade: {
+                        debug: false
+                    }
+                },
+
+                scene: [ZombbombArena],
+                backgroundColor: '#0000ff',
+
+                scale: {
+                    parent: "phaserapp",
+                    autoCenter: Phaser.Scale.Center.CENTER_BOTH,
+                    //mode: Phaser.Scale.FIT,
+                },
+            });
+    }
+}
+
+/* 
+GAME SCENES - BALL ARENA
+ * */
+class ZombbombArena extends Phaser.Scene {
+    graphics: Phaser.GameObjects.Graphics;
+
+    x: integer = 0;
+    y: integer = 0;
+
+    constructor() {
+        super({ key: 'ZombbombArena', active: true });
+    }
+
+    preload() {
+    }
+
+    create() {
+        this.graphics = this.add.graphics({ x: 0, y: 0 });
+    }
+
+    update() {
+        this.graphics.clear();
+        this.graphics.lineStyle(5, 0xffff00);
+        this.graphics.strokeCircle(this.x, this.y, 100);
+        this.x++;
+        this.y++;
+    }
+}
--- wwwroot/Zombbomb/gamelobby.js
+++ wwwroot/Zombbomb/gamelobby.js
@@ -7,3 +7,8 @@ document.getElementById("createroombutton").addEventListener("click", function (
     });
     event.preventDefault();
 });
+
+connection.on("StartGame", function () {
+    document.body.innerHTML = "<div id='controlbar' style=\"min-height:20px; height:2vh\"></div><div id='phaserapp' style=\"height:93vh\"></div>";
+    Game = new Zombbomb_Lobby_Game();
+});
