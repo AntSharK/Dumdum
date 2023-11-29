be6acf96be7b7849b75eeb65b0dcb9b3cc043720
Anthony Moh <great_ant@hotmail.com>
2023-11-29 01:34:34 -0800

    Placeholder for firing

--- scripts/Zombbomb/Zombbomb_Lobby_App.ts
+++ scripts/Zombbomb/Zombbomb_Lobby_App.ts
@@ -42,6 +42,10 @@ class ZombbombArena extends Phaser.Scene {
     desiredRotation: number;
     rotateLeft: boolean;
 
+    rotationSpeed: number = 0.15;
+    speed: number = 5;
+    fireOrderIssued: boolean;
+
     constructor() {
         super({ key: 'ZombbombArena', active: true });
     }
@@ -62,26 +66,44 @@ class ZombbombArena extends Phaser.Scene {
         this.desiredX = this.player.x;
         this.desiredY = this.player.y;
 
+        this.input.mouse.disableContextMenu();
         this.input.on('pointerdown', function (pointer) {
-            this.desiredX = pointer.x;
-            this.desiredY = pointer.y;
-            this.moveDirection = new Phaser.Math.Vector2(this.desiredX - this.player.x, this.desiredY - this.player.y);
-            this.moveDirection.normalize();
+            if (pointer.rightButtonDown()) {
+                this.desiredX = this.player.x;
+                this.desiredY = this.player.y;
+                this.moveDirection = new Phaser.Math.Vector2(pointer.x - this.player.x, pointer.y - this.player.y);
+                this.moveDirection.normalize();
 
-            this.desiredRotation = Math.atan2(this.moveDirection.y, this.moveDirection.x);
-            this.rotateLeft = ((this.desiredRotation > this.player.rotation && this.desiredRotation - this.player.rotation < Math.PI)
-                || (this.desiredRotation < this.player.rotation && this.player.rotation - this.desiredRotation > Math.PI)
-            );
+                this.desiredRotation = Math.atan2(this.moveDirection.y, this.moveDirection.x);
+                this.rotateLeft = ((this.desiredRotation > this.player.rotation && this.desiredRotation - this.player.rotation < Math.PI)
+                    || (this.desiredRotation < this.player.rotation && this.player.rotation - this.desiredRotation > Math.PI)
+                );
+
+                this.fireOrderIssued = true;
+            }
+            else { // Left click or anything else
+                this.desiredX = pointer.x;
+                this.desiredY = pointer.y;
+                this.moveDirection = new Phaser.Math.Vector2(this.desiredX - this.player.x, this.desiredY - this.player.y);
+                this.moveDirection.normalize();
+
+                this.desiredRotation = Math.atan2(this.moveDirection.y, this.moveDirection.x);
+                this.rotateLeft = ((this.desiredRotation > this.player.rotation && this.desiredRotation - this.player.rotation < Math.PI)
+                    || (this.desiredRotation < this.player.rotation && this.player.rotation - this.desiredRotation > Math.PI)
+                );
+            }
         }, this);
+
+        this.input.on
     }
 
     update() {
-        if (Math.abs(this.desiredRotation - this.player.rotation) > 0.25) {
+        if (Math.abs(this.desiredRotation - this.player.rotation) > this.rotationSpeed) {
             if (this.rotateLeft) {
-                this.player.rotation += 0.15;
+                this.player.rotation += this.rotationSpeed;
             }
             else {
-                this.player.rotation -= 0.15;
+                this.player.rotation -= this.rotationSpeed;
             }
         }
         else {
@@ -94,12 +116,20 @@ class ZombbombArena extends Phaser.Scene {
                 }
             }
 
-            if (Math.abs(this.desiredX - this.player.x) > 5) {
-                this.player.x += this.moveDirection.x * 5;
+            if (this.fireOrderIssued) { // TODO: Some stuff for firing - current logic is placeholder
+                this.player.x += this.moveDirection.x * this.speed * 50;
+                this.player.y += this.moveDirection.y * this.speed * 50;
+                this.fireOrderIssued = false;
+                this.desiredX = this.player.x;
+                this.desiredY = this.player.y;
             }
 
-            if (Math.abs(this.desiredY - this.player.y) > 5) {
-                this.player.y += this.moveDirection.y * 5;
+            if (Math.abs(this.desiredX - this.player.x) > this.speed) {
+                this.player.x += this.moveDirection.x * this.speed;
+            }
+
+            if (Math.abs(this.desiredY - this.player.y) > this.speed) {
+                this.player.y += this.moveDirection.y * this.speed;
             }
         }
     }
