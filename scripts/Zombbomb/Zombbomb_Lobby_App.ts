6f680fa5f1bc628100d7afd6db0d5f8d0df19e6d
Anthony Moh <great_ant@hotmail.com>
2023-11-29 01:18:51 -0800

    Initial prototype

--- scripts/Zombbomb/Zombbomb_Lobby_App.ts
+++ scripts/Zombbomb/Zombbomb_Lobby_App.ts
@@ -18,7 +18,7 @@ class Zombbomb_Lobby_Game {
                 },
 
                 scene: [ZombbombArena],
-                backgroundColor: '#0000ff',
+                backgroundColor: '#000000',
 
                 scale: {
                     parent: "phaserapp",
@@ -34,26 +34,77 @@ GAME SCENES - BALL ARENA
  * */
 class ZombbombArena extends Phaser.Scene {
     graphics: Phaser.GameObjects.Graphics;
+    player: Player;
 
-    x: integer = 0;
-    y: integer = 0;
+    desiredX: integer = 0;
+    desiredY: integer = 0;
+    moveDirection: Phaser.Math.Vector2;
+    desiredRotation: number;
+    rotateLeft: boolean;
 
     constructor() {
         super({ key: 'ZombbombArena', active: true });
     }
 
     preload() {
+        this.load.image('zombie', '/content/Zombbomb/ghost.png');
+        this.load.image('player', '/content/Zombbomb/pacman.png');
     }
 
     create() {
         this.graphics = this.add.graphics({ x: 0, y: 0 });
+
+        this.player = this.add.sprite(500, 500, 'player');
+        this.player.originX = this.player.width / 2;
+        this.player.originY = this.player.height / 2;
+        this.player.scale = 0.2;
+
+        this.desiredX = this.player.x;
+        this.desiredY = this.player.y;
+
+        this.input.on('pointerdown', function (pointer) {
+            this.desiredX = pointer.x;
+            this.desiredY = pointer.y;
+            this.moveDirection = new Phaser.Math.Vector2(this.desiredX - this.player.x, this.desiredY - this.player.y);
+            this.moveDirection.normalize();
+
+            this.desiredRotation = Math.atan2(this.moveDirection.y, this.moveDirection.x);
+            this.rotateLeft = ((this.desiredRotation > this.player.rotation && this.desiredRotation - this.player.rotation < Math.PI)
+                || (this.desiredRotation < this.player.rotation && this.player.rotation - this.desiredRotation > Math.PI)
+            );
+        }, this);
     }
 
     update() {
-        this.graphics.clear();
-        this.graphics.lineStyle(5, 0xffff00);
-        this.graphics.strokeCircle(this.x, this.y, 100);
-        this.x++;
-        this.y++;
+        if (Math.abs(this.desiredRotation - this.player.rotation) > 0.25) {
+            if (this.rotateLeft) {
+                this.player.rotation += 0.15;
+            }
+            else {
+                this.player.rotation -= 0.15;
+            }
+        }
+        else {
+            if (Math.abs(this.desiredRotation - this.player.rotation) > 0.05) {
+                if (this.rotateLeft) {
+                    this.player.rotation += 0.035;
+                }
+                else {
+                    this.player.rotation -= 0.035;
+                }
+            }
+
+            if (Math.abs(this.desiredX - this.player.x) > 5) {
+                this.player.x += this.moveDirection.x * 5;
+            }
+
+            if (Math.abs(this.desiredY - this.player.y) > 5) {
+                this.player.y += this.moveDirection.y * 5;
+            }
+        }
     }
+}
+
+class Player extends Phaser.GameObjects.Sprite {
+
 }
