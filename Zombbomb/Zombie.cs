15a6cc53ac3ea57aad64766854c3091d7155e4a8
Anthony Moh <great_ant@hotmail.com>
2023-12-01 23:20:35 -0800

    Add Zombbomb files (#121)

---
+++ Zombbomb/Zombie.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zombbomb
+{
+    public class Zombie
+    {
+        public string PlayerId;
+        public string ConnectionId;
+        public double LocationX;
+        public double LocationY;
+    }
+}
