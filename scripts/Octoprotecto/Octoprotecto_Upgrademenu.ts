272f3c0144cb22a8c0c43a0e62db6464ba057943
Anthony Moh <great_ant@hotmail.com>
2024-03-22 09:10:56 -0700

    Show dmg done

--- scripts/Octoprotecto/Octoprotecto_Upgrademenu.ts
+++ scripts/Octoprotecto/Octoprotecto_Upgrademenu.ts
@@ -170,7 +170,12 @@ class Upgradescreen extends Phaser.Scene {
             row.insertCell(0).textContent = "" + Math.round(selectedWeapon.projectileSpeed * 100) / 100;
             row.insertCell(0).textContent = "" + Math.round(selectedWeapon.projectileDamage * 100) / 100;
             row = table.insertRow(0);
+
             let cell = row.insertCell(0);
+            cell.rowSpan = 3;
+            cell.textContent = selectedWeapon.damageDealt + " DMG DONE";
+
+            cell = row.insertCell(0);
             cell.textContent = ">>";
             cell.title = "Refreshing will change the available purchaseable upgrades everywhere.";
             cell = row.insertCell(0);
