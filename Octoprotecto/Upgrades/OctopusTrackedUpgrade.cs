d05f0f731301c1f5482b94e8befda580f4734769
Anthony Moh <great_ant@hotmail.com>
2024-03-22 18:17:31 -0700

    Octopus body integrate upgrade (#182)
    * Octopus body integrate upgrade

--- Octoprotecto/Upgrades/OctopusTrackedUpgrade.cs
+++ Octoprotecto/Upgrades/OctopusTrackedUpgrade.cs
@@ -12,12 +12,12 @@
         public override int UpgradeIncrementCost => this.incrementCost;
         public override string UpgradeName => "octopustrackedupgrade";
 
-        private UpgradeType statUpgraded;
+        private UpgradeType upgradeType;
 
         public OctopusTrackedUpgrade(UpgradeType stat)
         {
-            this.statUpgraded = stat;
-            switch (this.statUpgraded)
+            this.upgradeType = stat;
+            switch (this.upgradeType)
             {
                 case UpgradeType.ArmorWhenHit:
                     this.displayName = "Toughen";
@@ -31,6 +31,13 @@
                     this.baseCost = 10;
                     this.incrementCost = 5;
                     break;
+                case UpgradeType.Integrate:
+                    this.displayName = "Integrate";
+                    this.description = "Resets upgrade costs";
+                    this.baseCost = 100;
+                    this.incrementCost = 0;
+                    this.MaxLimit = 1;
+                    break;
                 default:
                     this.displayName = "Unknown";
                     this.description = "Unknown";
@@ -42,12 +49,21 @@
         {
             base.ReadTargetProperties(octopus);
             var numberOfExistingUpgrades = octopus.TrackedUpgrades.Count(c => c.DisplayName == this.DisplayName);
-            this.description = this.description + " (owned: " + numberOfExistingUpgrades + ")";
+            this.description = this.description + " (owned: " + numberOfExistingUpgrades + (this.MaxLimit > 0 ? ("/" + this.MaxLimit + ")") : ")");
             this.Cost = this.UpgradeBaseCost + octopus.TrackedUpgrades.Count * this.UpgradeIncrementCost;
         }
 
         public override void ApplyUpgrade(Octopus octopus)
         {
+            switch (this.upgradeType)
+            {
+                case UpgradeType.Integrate:
+                    octopus.UpgradesApplied = -1;
+                    break;
+                default: // For everything else, the behavior is client-side
+                    break;
+            }
+
             octopus.TrackedUpgrades.Add(this); // All of the behavior is client-side and determined by the DisplayName
             base.ApplyUpgrade(octopus);
         }
@@ -56,6 +72,7 @@
         {
             ArmorWhenHit,
             PointsWhenHit,
+            Integrate,
         }
     }
 }
